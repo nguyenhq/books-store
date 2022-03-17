05baa6843d041fbed0f8cdd3ac545b98b8ec74b0
Huu Quoc NGUYEN <ng.h.quoc@gmail.com>
2022-03-17 17:37:42 +0100

    configure REST Apis for Read Only

--- src/main/java/com/phil/code/ecommerce/dao/ProductCategoryRepository.java
+++ src/main/java/com/phil/code/ecommerce/dao/ProductCategoryRepository.java
@@ -2,6 +2,8 @@ package com.phil.code.ecommerce.dao;
 
 import com.phil.code.ecommerce.entity.ProductCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
 }
--- src/main/java/com/phil/code/ecommerce/dao/ProductRepository.java
+++ src/main/java/com/phil/code/ecommerce/dao/ProductRepository.java
@@ -4,6 +4,5 @@ import com.phil.code.ecommerce.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
